d8951eb09dedc0869116166dc6cff41dba7c09d5
njuwangzhilong <njuwangzhilong@163.com>
2017-08-08 20:10:56 +0800

    wer

---
+++ CodeExamples/GetPathConstraint/test.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int add(int a,int b){
+	return a+b;
+}
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&a);
+	if(a>0){
+		a=add(a,23);
+		if(a%3==0){
+			b=1;
+		}else{
+			b=2;
+		}
+	}else{
+		if(a<-10){
+			b=3;
+		}else{
+			b=4;
+		}
+	}
+	return b;
+}
