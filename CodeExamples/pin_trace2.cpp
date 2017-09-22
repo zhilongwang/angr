88b1bcc40a8994dd4ec364dc81cb80eb142e21e1
njuwangzhilong <njuwangzhilong@163.com>
2017-09-22 16:32:28 +0800

    123

--- CodeExamples/pin_trace2.cpp
+++ CodeExamples/pin_trace2.cpp
@@ -2,6 +2,8 @@
 Intel Open Source License 
 Copyright (c) 2002-2015 Intel Corporation. All rights reserved.
  
+
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
 met:
